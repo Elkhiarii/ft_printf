5f42d0096514248fda74acded59ddcbc8d8ff743
Othmane elkhiari <othmaneelkhiari@gmail.com>
2022-11-29 18:40:09 +0100

    FIRST_push

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oelkhiar <oelkhiar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 17:49:07 by oelkhiar          #+#    #+#             */
+/*   Updated: 2022/11/29 18:31:35 by oelkhiar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h>
+# include <unistd.h>
+
+int	ft_putchr(char c);
+int	ft_putstr(char *s);
+
+#endif
