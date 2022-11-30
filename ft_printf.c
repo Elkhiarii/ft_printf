320317c9ec04deb18723648951b5b8dfae38fdd8
Othmane elkhiari <othmaneelkhiari@gmail.com>
2022-11-30 13:51:35 +0100

    FT_PRINTF

--- ft_printf.c
+++ ft_printf.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oelkhiar <oelkhiar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/29 17:49:07 by oelkhiar          #+#    #+#             */
-/*   Updated: 2022/11/30 13:42:48 by oelkhiar         ###   ########.fr       */
+/*   Created: 2022/11/30 13:39:36 by oelkhiar          #+#    #+#             */
+/*   Updated: 2022/11/30 13:43:00 by oelkhiar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdio.h>
-# include <unistd.h>
-
-int	ft_putchr(char c);
-int	ft_putstr(char *s);
-int	ft_putnbr(int c);
-int	ft_putnbr_xu(unsigned int c);
-int	ft_putnbr_xl(unsigned int c);
-int	ft_putnbr_u(unsigned int c);
-int	ft_printf(const char *s, ...);
-
-#endif
+int	ft_printf(const char *s, ...)
+{
+	
+ }
